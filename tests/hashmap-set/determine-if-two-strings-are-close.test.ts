0420ff19ff1bb21fec5a389a805f1395d8d191aa
AbdulRehman2236 <abdulrehmanfayyaz361@gmail.com>
2024-08-14 12:44:09 +0500

    Write test for problem to determine if two string are close or not

---
+++ tests/hashmap-set/determine-if-two-strings-are-close.test.ts
@@ -0,0 +1,17 @@
+import closeStrings from "../../src/hashmap-set/determine-if-two-strings-are-close";
+
+test("Returns true when both strings have same frequencies", () => {
+  expect(closeStrings("abc", "bca")).toEqual(true);
+});
+
+test("Returns false when one string has unique frequencies and the other doesn't", () => {
+  expect(closeStrings("a", "aa")).toEqual(false);
+});
+
+test("Returns true when both strings have the same frequencies and can be transformed into each other", () => {
+  expect(closeStrings("cabbba", "abbccc")).toEqual(true);
+});
+
+test("Returns false when the frequencies of characters in the two strings differ in an incompatible way", () => {
+  expect(closeStrings("aaabbbbccddeeeeefffff", "aaaaabbcccdddeeeeffff")).toEqual(false);
+});
